fb74e47a051cc4995c55ea082edf2e355b244d4d
Bill-Lyn <bill.lynch148@gmail.com>
2024-09-15 22:46:18 -0500

    Player Movement + animation
    made changes to the player node to enable movement as well as animation when moving

--- player.gd
+++ player.gd
@@ -1,30 +1,43 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+const speed = 100
+var is_moving = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+func _physics_process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	if (Input.is_action_pressed("ui_right")):
+		velocity.x = speed
+		velocity.y = 0
+	elif (Input.is_action_pressed("ui_left")):
+		velocity.x = -speed
+		velocity.y = 0
+	elif (Input.is_action_pressed("ui_down")):
+		velocity.x = 0
+		velocity.y = speed
+	elif (Input.is_action_pressed("ui_up")):
+		velocity.x = 0
+		velocity.y = -speed
 	else:
-		$AnimatedSprite2D.stop()
+		velocity.x = 0
+		velocity.y = 0
 		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	play_anim()
+	move_and_slide()
+	
+	#Fixes the issue of sprite pixels being spread to multiple pixels in the viewport
+	#Think it is because the position is stored as floats, so when the character is not
+	#"aligned" perfectly with the display pixels the render doubles some of them on the edges.
+	position.x = round(position.x)
+	position.y = round(position.y)
+	
+func play_anim():
+	var anim = $AnimatedSprite2D
+	
+	if (velocity.x == 0 and velocity.y == 0):
+		anim.play("idle")
+	else:
+		anim.play("walking")
+	
+	
